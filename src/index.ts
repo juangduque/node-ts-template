9ccd4d7fbca1987a22c29ea0932f73f9bc52f4b7
Juan Duque <juan.guillermo.duque@jeronimo-martins.com>
2023-10-30 09:44:09 -0500

    chore: add dev server

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
 
+const PORT = 3000;
 const app = express();
 
 app.get("/", (req, res) => {
   res.json({ message:"Hello World" });
 });
 
-app.listen(3000, () => {
-  console.log("Server is running");
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
